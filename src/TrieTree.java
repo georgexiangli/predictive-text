ecb0a477da19dd3249788d5ff6805c4f7db3ea8c
georgexiangli <ligeorge.x@gmail.com>
2020-08-10 21:31:01 -0500

    Update documentation

--- src/TrieTree.java
+++ src/TrieTree.java
@@ -4,18 +4,6 @@
 //
 // Author: George Li
 // Email: gli245@wisc.edu
-// Lecturer's Name: Andrew Kuemmel
-//
-///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
-//
-// Students who get help from sources other than their partner must fully
-// acknowledge and credit those sources of help here. Instructors and TAs do
-// not need to be credited here, but tutors, friends, relatives, room mates,
-// strangers, and others do. If you received no outside help from either type
-// of source, then please explicitly indicate NONE.
-//
-// Persons: None
-// Online Sources: None
 
 import java.util.ArrayList;
 // no other import statements are allowed
